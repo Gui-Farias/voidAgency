2457e2b11a609e7c113d16c92c4d22a03a0bfbe2
Gui Farias <gui.farias@outlook.com.br>
2025-01-23 20:07:20 -0300

    speed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,14 @@
 	import '../app.css';
 	let { children } = $props();
 
+	
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectSpeedInsights();
 </script>
 
 {@render children()}
