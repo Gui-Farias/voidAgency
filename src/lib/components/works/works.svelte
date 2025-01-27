60080105669bb41f460c0812ab2cc830dec7b150
Gui Farias <gui.farias@outlook.com.br>
2025-01-27 13:59:57 -0300

    canvas to mobile

--- src/lib/components/works/works.svelte
+++ src/lib/components/works/works.svelte
@@ -15,7 +15,7 @@
     { title: "Psicóloga Daniela Alves", link: 'https://danielapsicologia.com.br', imgDesk: pcDani, imgPhone: phoneDani, text: 'A saúde mental é muito importante e algo que todos nós precisamos. Este site é o primeiro passo para você agendar uma conversa com a psicóloga Daniela Alves.'},
     { title: "Joao Goes nutricionista", link: 'https://joao-goes.vercel.app',  imgDesk: pcJoao, imgPhone: phoneJoao, text: 'Crie planos alimentares para alcançar a sua meta, cuide da sua saúde com a orientação do nutricionista João Goes.'},
     { title: "Octa Studio", link: 'https://octa-estudio.vercel.app', imgDesk: pcOcta, imgPhone: phoneOcta, text: 'Temos como principais objetivos incentivar e dar espaço para novos artistas, contribuir para o crescimento de artistas independentes e, além disso, auxiliar todos aqueles que querem viver da música.'},
-    { title: "Gui Farias", link: 'https://guifarias.com.br', imgDesk: pcGui, imgPhone: phoneGui, text: 'Criar um portfólio online onde você possa mostrar seu portfólio e serviços é sempre uma boa opção para encontrar um emprego ou atrair novos clientes.'}
+    { title: "Gui Farias", link: 'https://guifarias.com.br', imgDesk: pcGui, imgPhone: phoneGui, text: 'Criar um portfólio online onde você possa mostrar seus trabalhos e serviços é sempre uma boa opção para encontrar um emprego ou atrair novos clientes.'}
   ];
 
   // Função para embaralhar o array
