ddb85c998d49eba3f96867d48b87c848c80f7fe9
Gui Farias <gui.farias@outlook.com.br>
2025-03-31 14:46:36 -0300

    video mobile

--- src/lib/components/digital/digital.svelte
+++ src/lib/components/digital/digital.svelte
@@ -1,6 +1,6 @@
 <script>
-  import { t } from 'svelte-i18n';
   import { onMount } from "svelte";
+  import { t } from 'svelte-i18n';
 
   import digi01 from '$lib/assets/digi01.svg'
   import digi02 from '$lib/assets/digi02.svg'
@@ -8,65 +8,37 @@
   import digi04 from '$lib/assets/digi04.svg'
   import digi05 from '$lib/assets/digi05.svg'
 
-  
-  // Lista de imagens que você quer trocar ao rolar a página
-  const images = [
-    digi01,
-    digi02,
-    digi03,
-    digi04,
-    digi05,
-  ];
+  const images = [digi01, digi02, digi03, digi04, digi05];
+  let currentImage = images[0];  
+  let isVisible = null;
+  let elements = [];
 
-  let currentImage = images[0];  // A imagem inicial
-
-  // Função para lidar com a mudança de imagem
   const changeImage = (index) => {
     currentImage = images[index];
   };
 
-  onMount(() => {
-  const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: window.matchMedia("(min-width: 768px)").matches ? 0.8 : 0.5, // O elemento precisa estar 100% visível
-  };
+ onMount(() => {
+    const isDesktop = window.matchMedia("(min-width: 768px)").matches;
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: isDesktop ? 0.8 : 0.4,
+    };
 
-  let lastVisibleIndex = null; // Rastreia o último elemento visível
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      const index = parseInt(entry.target.getAttribute('data-index'), 10);
-
-      if (entry.isIntersecting) {
-        // Quando o elemento fica visível
-        entry.target.classList.add('opacity-100');
-        entry.target.classList.remove('opacity-0');
-
-        // Se existir um elemento anteriormente visível, remova sua opacidade
-        if (lastVisibleIndex !== null && lastVisibleIndex !== index) {
-          const previousElement = document.querySelector(`[data-index="${lastVisibleIndex}"]`);
-          if (previousElement) {
-            previousElement.classList.remove('opacity-100');
-            previousElement.classList.add('opacity-0');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        const index = parseInt(entry.target.getAttribute('data-index'), 10);
+        if (entry.isIntersecting) {
+          isVisible = index;
+          if (isDesktop) {
+            changeImage(index);
           }
         }
+      });
+    }, options);
 
-        // Atualize o índice do último elemento visível
-        lastVisibleIndex = index;
-
-
-        if(window.matchMedia("(min-width: 768px)").matches) {
-          changeImage(index);
-        }
-      }
-    });
-  }, options);
-
-    // Observa os títulos
-    const targets = document.querySelectorAll('.digiText');
-    targets.forEach(target => observer.observe(target));
-});
+    elements.forEach((el) => observer.observe(el));
+  });
 </script>
 
 <section class="md:flex md:justify-between lg:justify-evenly -mt-20">
@@ -75,7 +47,21 @@
     <img src={currentImage} alt="" aria-hidden="true" class="w-full transition-all max-w-sm mx-auto mt-[10vh] rounded-xl" />
   </div>
 
+
   <div class="text md:w-2/5 mt-40 md:pb-32">
+    {#each [0, 1, 2, 3, 4] as index}
+      <p bind:this={elements[index]} class="digiText transition-all pt-[10vh] pb-[20vh]  {index == 0 && 'pt-0'} {index == 4 && 'pb-0'}" data-index={index}
+         class:opacity-100={isVisible === index} class:opacity-0={isVisible !== index}>
+        {$t(`about.text${index + 1}`)}
+      </p>
+      {#if index == 4} 
+        <a href="https://api.whatsapp.com/send?phone=5511910529070&text=Olá,%20estou%20vindo%20do%20seu%20site%20e%20gostaria%20..." target="_blank" rel="noopener noreferrer" class:opacity-100={isVisible === index} class:opacity-0={isVisible !== index} class="btn block">{$t('btn.especialista')}</a>
+      {/if}
+    {/each}
+  </div>
+
+
+  <!-- <div class="text md:w-2/5 mt-40 md:pb-32">
     <p class="digiText transition-all opacity-100 pb-[25vh]" data-index="0" >{$t('about.text1')}</p>
     <p class="digiText transition-all opacity-0 pt-[12vh] pb-[15vh]" data-index="1" >{$t('about.text2')}</p>
     <p class="digiText transition-all opacity-0 pt-[10vh] pb-[15vh]" data-index="2" >{$t('about.text3')}</p>
@@ -84,21 +70,21 @@
       <p>{$t('about.text5')}</p>
       <a href="https://api.whatsapp.com/send?phone=5511910529070&text=Olá,%20estou%20vindo%20do%20seu%20site%20e%20gostaria%20..." target="_blank" rel="noopener noreferrer" class="btn block">{$t('btn.especialista')}</a>
     </div>
-  </div>
+  </div> -->
 </section>
 
 <style>
-  /* Estilo inicial: invisível com blur */
 .opacity-0 {
   opacity: 0;
-  filter: blur(10px); /* Aplica o blur inicial */
-  transition: opacity 0.6s ease-in-out, filter 0.6s ease-in-out; /* Transição suave */
+  filter: blur(10px); 
+  transition: opacity 0.6s ease-in-out, filter 0.6s ease-in-out;
 }
 
-/* Estilo final: visível sem blur */
 .opacity-100 {
   opacity: 1;
-  filter: blur(0px); /* Remove o blur */
-  transition: opacity 0.6s ease-in-out, filter 0.6s ease-in-out; /* Transição suave */
+  filter: blur(0px); 
+  transition: opacity 0.6s ease-in-out, filter 0.6s ease-in-out;
 }
 </style>
+
+
--- src/lib/components/video/video.svelte
+++ src/lib/components/video/video.svelte
@@ -5,32 +5,15 @@
   import videoCelWeb from '$lib/assets/voidAgencyMobile.webm'
 
   import { onMount } from "svelte";
-  let isTouchDevice = false;
-  let isMobile = false;
-
-  onMount(() => {
-    isTouchDevice = "ontouchstart" in window || navigator.maxTouchPoints > 0;
-    isMobile = window.matchMedia("(max-width: 768px)").matches;
-  });
-
 </script>
 
-<section class="flex justify-center mt-28 mb-44">
-<video controls playsinline class="w-full max-h-[80vh] rounded-2xl shadow-lg mx-auto">
-  {#if isMobile}
-    {#if isTouchDevice}
-      <source src="{videoCel}" type="video/mp4" media="(max-width: 768px)" />
-      <source src="{videoCelWeb}" type="video/webm" media="(max-width: 768px)" />
-    {:else}
-      <source src="{videoPc}" type="video/mp4" />
-      <source src="{videoPcWeb}" type="video/webm" />
-    {/if}
-  {:else}
-    <source src="{videoPc}" type="video/mp4" />
-    <source src="{videoPcWeb}" type="video/webm" />
-  {/if}
-
-  <track kind="captions" src="" srclang="pt" label="PT" />
-  Seu navegador não suporta vídeos.
-</video>
+<section class="flex justify-center my-44">
+  <video controls playsinline class="w-full max-h-[80vh] rounded-2xl shadow-lg mx-auto">
+    <source src={videoCel} type="video/mp4" media="(max-width: 640px)" />
+    <source src={videoCelWeb} type="video/webm" media="(max-width: 640px)" />
+    <source src={videoPc} type="video/mp4" />
+    <source src={videoPcWeb} type="video/webm" />
+    <track kind="captions" src="" srclang="pt" label="PT" />
+    Seu navegador não suporta vídeos.
+  </video>
 </section>
