e86293db1c2d8caa65b9ab013e9c343132ec0ac1
Gui Farias <gui.farias@outlook.com.br>
2025-04-02 20:04:58 -0300

    video poster

--- src/lib/components/video/video.svelte
+++ src/lib/components/video/video.svelte
@@ -3,12 +3,13 @@
   import videoPcWeb from '$lib/assets/voidAgency.webm'
   import videoCel from '$lib/assets/voidAgencyMobile.mp4'
   import videoCelWeb from '$lib/assets/voidAgencyMobile.webm'
+  import poster from '$lib/assets/poster.webp'
 
   import { onMount } from "svelte";
 </script>
 
 <section class="flex justify-center my-44">
-  <video controls playsinline class="w-full max-h-[80vh] rounded-2xl shadow-lg mx-auto">
+  <video controls poster={poster} playsinline class="w-full max-h-[80vh] rounded-2xl shadow-lg mx-auto">
     <source src={videoCel} type="video/mp4" media="(max-width: 640px)" />
     <source src={videoCelWeb} type="video/webm" media="(max-width: 640px)" />
     <source src={videoPc} type="video/mp4" />
